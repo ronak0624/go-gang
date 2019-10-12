147363e9809d6df3cf52ded3026c779da5133a55
Roger Zavala <Rogeliozavala15@gmail.com>
2019-10-12 13:59:16 -0700

    removed requried files from code to get basic server running

--- server.js
+++ server.js
@@ -5,12 +5,6 @@ var express = require("express");
 var PORT = process.env.PORT || 8080;
 var app = express();
 
-// Requiring our models for syncing
-var db = require("./models/index")
-
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
-
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,11 +14,6 @@ var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-// Import routes and give the server access to them.
-var routes = require("./routes/html-routes");
-
-app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
