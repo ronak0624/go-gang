68f6f89af0bf59010a36bec600d9ad0b493c9a8b
amberlee <amberleeha@gmail.com>
2019-10-14 14:10:42 -0700

    public js files

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ app.set("view engine", "handlebars");
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
 });
