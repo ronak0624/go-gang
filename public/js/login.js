5bcef2a378d86143ed8e0f432db5d216d088eb4b
Roger Zavala <Rogeliozavala15@gmail.com>
2019-10-15 13:54:59 -0700

    grabbing username and password info

--- public/js/login.js
+++ public/js/login.js
@@ -1,17 +1,18 @@
-$(document).ready(function (event) {
-    event.preventDefault();
+$(document).ready(function(){
 
     $("#login").on("submit", function(event){
         event.preventDefault();
+
         //grabs the values from username and password
         var loginInfo = {
-            userName:$("#username").val(),
-            password:$("#password").val()
+            userName:$("#inputUsername").val().trim(),
+            password:$("#inputPassword").val().trim()
         }
         //sending user loginInfo data 
-     $.post("/api/login",loginInfo).then(function(data){
+     $.post("/auth/login",loginInfo).then(function(data){
         console.log(data)
-     })
+    })
+    console.log(loginInfo)
     })
 
 });
