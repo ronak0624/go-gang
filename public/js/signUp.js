7c9a7d441f201b217a718d0237e88d62cca35eab
Roger Zavala <Rogeliozavala15@gmail.com>
2019-10-16 11:38:02 -0700

    making changes

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -4,22 +4,19 @@ $(document).ready(function () {
         event.preventDefault();
         console.log("im working")
         var newUser = {
-            firstName: $("#inputFirst4").val().trim(),
-            lastName: $("#inputLast4").val().trim(),
-            DOB: $("#inputDOB4").val()
-        }
-        var userAnswers = {
-            question1:$("#exampleFormControlSelect1").val(),
+            name: $("#inputFirst4").val().trim() + " " + $("#inputLast4").val().trim(),
+            DOB: $("#inputDOB4").val(),
+            question1: $("#exampleFormControlSelect1").val(),
             answer1: $("#exampleFormControlTextarea1").val().trim(),
-            question2:$("#exampleFormControlSelect1").val(),
+            question2: $("#exampleFormControlSelect1").val(),
             answer2: $("#exampleFormControlTextarea2").val().trim(),
             answer3: $("#exampleFormControlTextarea3").val().trim(),
-            question3:$("#exampleFormControlSelect1").val()
+            question3: $("#exampleFormControlSelect1").val()
         }
 
-        $.post("/auth/signUp", newUser, userAnswers).then(function (data) {
+        $.post("/auth/signUp", newUser).then(function (data) {
             console.log(data)
         })
-        console.log(newUser, userAnswers);
+        // console.log(newUser);
     })
 });
